bcab3e540e044630311d3b151ea386bb7349f335
neomond <neoweise@yahoo.com>
2025-01-23 22:34:49 +0400

    BSTs add

---
+++ BSTs.playground/Contents.swift
@@ -0,0 +1,9 @@
+// MARK: - BINARY SEARCH TREES: SWIFT
+
+// A binary search tree is a data structure that is comprised of nodes in a branching relationship, each node having a key signifying its value.
+
+// The basic rules are:
+// The left child and its subtree contain values less than the parent node’s values
+// The right child and its subtree contain values greater than the parent node’s values
+// Each left and right subtree is in itself an implementation of a BST
+
